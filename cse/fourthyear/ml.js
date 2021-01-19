f60c98153ab484a4d2a95e1a272dd93d8002942a
Charan Potnuru <76473485+BhaskarCharan@users.noreply.github.com>
2021-01-19 15:29:10 +0530

    Add files via upload

---
+++ cse/fourthyear/ml.js
@@ -0,0 +1,19 @@
+var app = angular.module('MyApp', [])
+        app.controller('MyController', function ($scope, $window) {
+            $scope.subject = [
+                
+				 
+				 
+                 { Name: "UNIT-4 ", video: " https://www.youtube.com/playlist?list=PL6jBdoOg0AGcUORMVM312czbvPpQYpajO" },
+				 
+				  { Name: "UNIT-5 ", video: "https://www.youtube.com/playlist?list=PL6jBdoOg0AGcUORMVM312czbvPpQYpajO"},
+               
+				
+               
+               
+                ];
+ 
+            
+ 
+           
+        });
---
+++ cse/thirdyear/usp.js
@@ -0,0 +1,47 @@
+var app = angular.module('MyApp', [])
+        app.controller('MyController', function ($scope, $window) {
+            $scope.subject = [
+                { Name: " UNIT-1 Lecture 1 ", video: "https://www.youtube.com/watch?v=qvFtporw75" },
+				{ Name: " UNIT-1 Lecture  2", video: "https://www.youtube.com/watch?v=hqHph1A6A6I" },
+				{ Name: " UNIT-1 Lecture  3", video: "https://www.youtube.com/watch?v=T2Rf1WDtjcs" },
+				{ Name: " UNIT-1 Lecture  4", video: "https://www.youtube.com/watch?v=3uIFWDOpqI4" },
+				{ Name: " UNIT-1 Lecture  5", video: "https://www.youtube.com/watch?v=ffeqCknG7Eo&t=1510s" },
+				
+				
+                { Name: " UNIT-2  Lecture 1", video: "https://www.youtube.com/watch?v=jP1wdxBAY6s" },
+                { Name: " UNIT-2 Lecture 2", video: "https://www.youtube.com/watch?v=jHFjMw7SRZc" },
+				{ Name: " UNIT-2 Lecture 3", video: "https://www.youtube.com/watch?v=co6m73_WbWU" },
+				{ Name: " UNIT-2 Lecture 4", video: "https://www.youtube.com/watch?v=mpwjoucO83o&t=366s" },
+				
+                { Name: " UNIT-4 Lecture 1 ", video: "https://www.youtube.com/watch?v=iQaHa7ZrGVk&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=23" },
+				{ Name: " UNIT-4 Lecture 2", video: "https://www.youtube.com/watch?v=lYKFJFp7omQ&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=26" },
+				{ Name: " UNIT-4 Lecture 3", video: "https://www.youtube.com/watch?v=xq1B_hiEO_4&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=24" },
+				{ Name: " UNIT-4 Lecture 4", video: "https://www.youtube.com/watch?v=YoSYfDtVkdY&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=25" },
+				{ Name: " UNIT-4 Lecture 5", video: "https://www.youtube.com/watch?v=YT_GGIeDj1E&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=31" },
+				{ Name: " UNIT-4 Lecture 6", video: "https://www.youtube.com/watch?v=dYctGiDwg7o&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=32" },
+				{ Name: " UNIT-4 Lecture 7", video: "https://www.youtube.com/watch?v=odYk5GG1AjQ&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=29" },
+				
+				
+				 { Name: "UNIT-3 Lecture 1", video: "https://www.youtube.com/watch?v=JPHT0I6WONE"},
+				 { Name: "UNIT-3 Lecture 2", video: "https://www.youtube.com/watch?v=_CA9Zyl-GPM&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=12"},
+				 { Name: "UNIT-3 Lecture 3", video: "https://www.youtube.com/watch?v=frIqBLjEdiI&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=13"},
+				 { Name: "UNIT-3 Lecture 4", video: "https://www.youtube.com/watch?v=iQWt5MDz1uk&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=15"},
+				 { Name: "UNIT-3 Lecture 5", video: "https://www.youtube.com/watch?v=_lNR72V3uls&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=16"},
+				 { Name: "UNIT-3 Lecture 6", video: "https://www.youtube.com/watch?v=8eHQwGodNbk&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=17"},
+				 { Name: "UNIT-3 Lecture 7" , video: "https://www.youtube.com/watch?v=4WX2X2DisC8&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=20"},
+				 { Name: "UNIT-3 Lecture 8", video: "https://www.youtube.com/watch?v=-cW0LPFyAy0&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=19"},
+				 { Name: "UNIT-3 Lecture 9" , video: "https://www.youtube.com/watch?v=9OCKD6YS5C8&list=PLflAM0ONZwu5JJhKOPbWV8PPgB68pY5o5&index=21"},
+				 
+				  { Name: "UNIT-5 Lecture 1", video: "https://www.youtube.com/watch?v=mQV1RdIDRLA"},
+				 { Name: "UNIT-5 Lecture 2", video: "https://www.youtube.com/watch?v=3dx4fMMtoR4"},
+				
+               
+				
+               
+               
+                ];
+ 
+            
+ 
+           
+        });
