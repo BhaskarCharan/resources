1e5e7c0f8decd22e32c736929181aa554928f2e7
ChittiAkhil <76650997+ChittiAkhil@users.noreply.github.com>
2021-01-26 00:22:41 -0600

    Add files via upload

---
+++ mec/secondyear/mos.js
@@ -0,0 +1,21 @@
+var app = angular.module('MyApp', [])
+        app.controller('MyController', function ($scope, $window) {
+            $scope.subject = [
+                { Name: " UNIT-1 ", video: "http://bit.ly/mos_me" },
+                
+                { Name: " UNIT-2 ", video: "http://bit.ly/mos_me" },
+				
+		{ Name: " UNIT-3 ", video: "http://bit.ly/mos_me" },
+
+                { Name: " UNIT-4 ", video: "http://bit.ly/mos_me" },
+
+                { Name: " UNIT-5 ", video: "http://bit.ly/mos_me" },
+                
+                
+               
+                ];
+ 
+            
+ 
+           
+        });
